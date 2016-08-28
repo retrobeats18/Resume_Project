66bd7cdf0a2eac96832ec26b16a80bb54fca018f
Jordan <jordancstrickland@gmail.com>
2016-08-27 23:01:03 -0400

    resumeBuilder.js
    deleted unnecessary semicolons

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -302,7 +302,7 @@ function inName(name) {
     name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
 
     return name[0] + " " + name[1];
-};
+}
 
 $('#main').append(internationalizeButton);
 
